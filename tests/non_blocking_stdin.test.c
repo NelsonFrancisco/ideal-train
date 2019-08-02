f0e99a4756d2bb7c74a3db9d25df487ea7e34add
Nelson Francisco <nelson_cunha1991@hotmail.com>
2019-08-02 13:16:23 +0100

    qa: Testing framework setup

---
+++ tests/non_blocking_stdin.test.c
@@ -0,0 +1,21 @@
+#include "../Unity/unity.h"
+#include "../src/non_blocking_stdin.h"
+
+void setUp(void)
+{
+}
+
+void tearDown(void)
+{
+}
+
+void shouldReadStream(void)
+{
+}
+
+int main(void)
+{
+  UNITY_BEGIN();
+  RUN_TEST(shouldReadStream);
+  return UNITY_END();
+}
